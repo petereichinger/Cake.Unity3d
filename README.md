8b54f0115f41d346e5187abbcc2e3f62f69f8cff
Peter Eichinger <peter.eichinger@gmail.com>
2018-01-09 22:54:11 +0100

    Initial basic version

---
+++ README.md
@@ -0,0 +1,5 @@
+# Cake.Unity3d
+
+This is a work in progress addin for [Cake](https://cakebuild.net/) that adds the ability to build [Unity 3D](https://unity3d.com/) projects.
+
+This is not really useable at the moment.
---
+++ src/Cake.Unity3d/UnityBuild.cs
@@ -0,0 +1,29 @@
+﻿using Cake.Core;
+using Cake.Core.Annotations;
+using Cake.Core.IO;
+
+namespace Cake.Unity3d {
+    public static class Build {
+        [CakeMethodAlias]
+        public static void UnityBuild(this ICakeContext context, DirectoryPath projectPath, FilePath target, FilePath unity) {
+            var builder = new ProcessArgumentBuilder();
+
+            builder.Append("-batchmode");
+            builder.Append("-quit");
+
+            builder.Append("-buildWindows64Player");
+            builder.AppendQuoted(target.MakeAbsolute(context.Environment).FullPath);
+
+            builder.Append("-projectPath");
+            builder.AppendQuoted(projectPath.MakeAbsolute(context.Environment).FullPath);
+
+            builder.Append("-logFile");
+            builder.AppendQuoted(projectPath.CombineWithFilePath(new FilePath("build.log")).FullPath);
+
+            var runner = new ProcessRunner(context.Environment,context.Log);
+            var settings = new ProcessSettings {Arguments = builder, RedirectStandardOutput = true, RedirectStandardError = true};
+            var process = runner.Start(unity, settings);
+            process.WaitForExit();
+        }
+    }
+}
